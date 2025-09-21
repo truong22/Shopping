8faa34274629119097c56cfba82bf687028250d1
truong22 <buidinhtruong2201@gmail.com>
2025-09-22 00:47:32 +0700

    CategoryReponsitory

--- src/main/java/com/dailycodework/dreamshops/Exception/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.dailycodework.dreamshops.Exception;
-
-public class AlreadyExistsException extends RuntimeException  {
-    public AlreadyExistsException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/dailycodework/dreamshops/Exception/ResourcetNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dailycodework.dreamshops.Exception;
-
-public class ResourcetNotFoundException extends  RuntimeException{
-    public ResourcetNotFoundException(String message){
-        super(message);
-    }
-}
