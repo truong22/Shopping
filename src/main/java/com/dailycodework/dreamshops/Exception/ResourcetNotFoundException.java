85567e84df707f4e90cacf264709ca67fde225a3
truong22 <buidinhtruong2201@gmail.com>
2025-09-22 00:49:10 +0700

    exception

---
+++ src/main/java/com/dailycodework/dreamshops/Exception/ResourcetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.dailycodework.dreamshops.Exception;
+
+public class ResourcetNotFoundException extends  RuntimeException{
+    public ResourcetNotFoundException(String message){
+        super(message);
+    }
+}
