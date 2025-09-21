27238197dd2130ebbec94facbcdec1d3c65dc3d2
truong22 <buidinhtruong2201@gmail.com>
2025-09-22 00:45:35 +0700

    ICategoryService

--- src/main/java/com/dailycodework/dreamshops/entity/Image.java
+++ src/main/java/com/dailycodework/dreamshops/entity/Image.java
@@ -1,4 +1,4 @@
-package com.dailycodework.dreamshops.dto;
+package com.dailycodework.dreamshops.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
---
+++ src/main/java/com/dailycodework/dreamshops/service/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.dailycodework.dreamshops.service;
+
+import com.dailycodework.dreamshops.entity.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+
+    Category getCategoryById(Long id);
+    Category getCategorybyName(String name);
+    List<Category> getCategoryByAll();
+    void deleteCategory(Long id);
+    Category addCategory(Category category);
+    Category updateCategory(Category category,Long id);
+}
